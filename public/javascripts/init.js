1a4bd733dd62ca70b7a79782188ff380a009f357
jscadden <jscadden@wagileconsulting.com>
2010-04-23 15:37:04 -0600

    fixed error with nodeJsonString

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -43,7 +43,7 @@ $(document).ready(function() {
 
 function stompOnMessageFrame(frame) {
 	$.fancybox.close();
-	var nodeJson = eval('(' + nodeJsonString + ')').node;
+	var nodeJson = eval('(' + frame.body + ')').node;
 	addNode(nodeJson);
 }
 
