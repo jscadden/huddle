25ebd92a1a300ff6f6c45ff2f9dfa1fc89391d44
Eric Wollesen <ericw@xmtp.net>
2010-07-14 12:36:29 -0600

    Reorg js init

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,48 +1,58 @@
 var nodeIdAdded = undefined;
 
 $(document).ready(function() {
-	//$("#node_details").hide();
-	$("#nodes").huddle($('canvas'), {
-		width: 240,
-		height: 240,
-		onNodeSelected: nodeSelected
-	});
-	
-	fdSliderController.removeOnLoadEvent();
-	var options = {
-	        // A reference to the input
-	        inp:            $("#slider").get(0),
-	        // A String containing the increment value (and the return precision, in this case 2 decimal places "x.20")
-	        inc:            "0.1",
-	        // Maximum keyboard increment (automatically uses double the normal increment if not given)
-	        maxInc:         "0.1",
-	        // Numeric range
-	        range:          [0.1,2],
-	        // Callback functions
-	        callbacks:      { "update":[updateZoom], "create":[updateZoom] },
-	        // String representing the classNames to give the created slider
-	        classNames:     "horizontalclass",
-	        // Tween the handle onclick?
-	        tween:          false,
-	        // Is this a vertical slider
-	        vertical:       true,
-	        // Do we hide the associated input on slider creation
-	        hideInput:      true,
-	        // Does the handle jump to the nearest click value point when the bar is clicked (tween cannot then be true)
-	        clickJump:      true,
-	        // Full ARIA required
-	        fullARIA:       false,
-	        // Do we disable the mouseWheel for this slider
-	        noMouseWheel:   false
-	};
-	fdSliderController.createSlider(options);
-	
-	$("#nodes_wrapper .reply a").fancybox({
-		'hideOnContentClick': false,
-		'scrolling': 'no',
-		'titleShow': false
-	});
+	init_huddle_canvas();
+	init_huddle_scaling_slider();
+	init_new_node_fancy_box();
 });
+
+function init_huddle_canvas() {
+    $("#nodes").huddle($('canvas'), {
+	    width: 240,
+	    height: 240,
+	    onNodeSelected: nodeSelected
+    });
+}
+
+function init_huddle_scaling_slider() {
+    var options = {
+	// A reference to the input
+	inp:            $("#slider").get(0),
+	// A String containing the increment value (and the return precision, in this case 2 decimal places "x.20")
+	inc:            "0.1",
+	// Maximum keyboard increment (automatically uses double the normal increment if not given)
+	maxInc:         "0.1",
+	// Numeric range
+	range:          [0.1,2],
+	// Callback functions
+	callbacks:      { "update":[updateZoom], "create":[updateZoom] },
+	// String representing the classNames to give the created slider
+	classNames:     "horizontalclass",
+	// Tween the handle onclick?
+	tween:          false,
+	// Is this a vertical slider
+	vertical:       true,
+	// Do we hide the associated input on slider creation
+	hideInput:      true,
+	// Does the handle jump to the nearest click value point when the bar is clicked (tween cannot then be true)
+	clickJump:      true,
+	// Full ARIA required
+	fullARIA:       false,
+	// Do we disable the mouseWheel for this slider
+	noMouseWheel:   false
+    };
+
+    fdSliderController.removeOnLoadEvent();
+    fdSliderController.createSlider(options);
+}
+
+function init_new_node_fancy_box() {
+    $("#nodes_wrapper .reply a").fancybox({
+	'hideOnContentClick': false,
+	'scrolling': 'no',
+	'titleShow': false
+    });
+}
 
 function closeAddNodeForm(request) {
 	nodeIdAdded = request.responseText;
@@ -50,7 +60,6 @@ function closeAddNodeForm(request) {
 }
 
 function stompOnMessageFrame(frame) {
-	//$.fancybox.close();
 	var nodeJson = eval('(' + frame.body + ')').node;
 	addNode(nodeJson);
 }
