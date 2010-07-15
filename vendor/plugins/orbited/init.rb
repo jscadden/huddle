273c917e015345160348a66e4e373132f9b40606
Eric Wollesen <ericw@xmtp.net>
2010-07-15 11:04:16 -0600

    orbited.yml is needed by the orbited plugin, and hence needed when deployed to heroku.  However, we need it to be parameterizable in development, so we'll pull from environment vars for development.
    There are no passwords saved in orbited.yml, so it's ok to have it in source control.
    
    For local dev use, modify your apache's virtualhost config to SetEnv the appropriate settings.

--- vendor/plugins/orbited/init.rb
+++ vendor/plugins/orbited/init.rb
@@ -1,8 +1,8 @@
 require 'yaml'
 require 'ostruct'
+require 'erb'
 
-::OrbitedConfig = OpenStruct.new(YAML.load_file(File.join(RAILS_ROOT, 'config', 'orbited.yml'))[RAILS_ENV])
-
+::OrbitedConfig = OpenStruct.new(YAML.load(ERB.new(File.open(File.join(Rails.root, 'config', 'orbited.yml')).read).result)[Rails.env])
 ActionView::Base.send :include, OrbitedHelper
 
 Orbited.set_defaults
