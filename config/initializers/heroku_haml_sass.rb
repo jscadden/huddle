dfb501769e01e95789e55af2b8a7f703627f323f
Eric Wollesen <ericw@xmtp.net>
2010-07-15 16:10:26 -0600

    It didn't work.  I'll play with this later...  maybe.
    Revert "The last commit works, but I'm masochistic"
    
    This reverts commit c54c51dac61708ad68317fb6a3fa414012545566.

--- config/initializers/heroku_haml_sass.rb
+++ config/initializers/heroku_haml_sass.rb
@@ -1,19 +1,6 @@
 require "fileutils"
 
-module HerokuCheck
-  def heroku?
-    !!ENV["HEROKU_TYPE"]
-  end
-end
-Rails.send(:extend, HerokuCheck)
-
-
-if Rails.heroku?
+if !ENV["HEROKU_TYPE"].blank?
   FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets", "compiled"))
   ActionController::Dispatcher.middleware.use(Rack::Static, :root => "tmp/", :urls => ["/stylesheets/compiled"])
-
-  Sass::Plugin.options[:template_location] = {
-    "app/stylesheets" => "tmp/stylesheets/compiled"
-  }
 end
-
