35a5e043a852f9f5a6d52ec065bff5a138a4e998
Eric Wollesen <ericw@xmtp.net>
2010-07-15 14:13:08 -0600

    http://github.com/pedro/hassle/issues/closed#issue/4

---
+++ config/initializers/heroku_haml_sass.rb
@@ -0,0 +1,33 @@
+# adapted from 
+# http://github.com/pedro/hassle/issues/closed#issue/4 (follow Mislav's code)
+
+require 'haml/helpers/action_view_mods'
+require 'haml/helpers/action_view_extensions'
+require 'haml/template'
+require 'sass'
+require 'sass/plugin'
+
+Rails::Configuration.class_eval do
+  attr_writer :heroku
+
+  def initialize
+    super
+    @heroku = !!ENV['HEROKU_TYPE']
+  end
+
+  def heroku?
+    @heroku
+  end
+end
+
+# based on suggestion from Chris Eppstein:
+# http://github.com/chriseppstein/compass/issues/issue/130/#comment_238101
+Sass::Plugin.options[:css_location] = [
+  Rails.configuration.heroku? ? "#{Rails.root}/tmp/stylesheets" : "#{Rails.root}/public/stylesheets"
+]
+
+if Rails.configuration.heroku? 
+  # add Rack middleware to serve compiled stylesheets from "tmp/stylesheets"
+  Rails.configuration.middleware.insert_after 'Sass::Plugin::Rack', 'Rack::Static',
+:urls => ['/stylesheets'], :root => "#{Rails.root}/tmp"
+end
