98800b4b975f8913d04103b9fb89e36ea1e42fd4
Eric Wollesen <ericw@xmtp.net>
2010-07-14 18:48:26 -0600

    New node list, linked to the huddle, and vice-versa

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -4,14 +4,16 @@ $(document).ready(function() {
 	init_huddle_canvas();
 	init_huddle_scaling_slider();
 	init_new_node_fancy_box();
+	init_node_list();
 });
 
 function init_huddle_canvas() {
     $("#nodes").huddle($('canvas'), {
 	    width: 240,
 	    height: 240,
-	    onNodeSelected: nodeSelected
+	    onNodeSelected: nodeSelected // called when huddle is loaded
     });
+    $("#huddle_wrapper").css("position", "fixed");
 }
 
 function init_huddle_scaling_slider() {
@@ -52,6 +54,27 @@ function init_new_node_fancy_box() {
 	'scrolling': 'no',
 	'titleShow': false
     });
+    $(".node a.reply").fancybox({
+	'hideOnContentClick': false,
+	'scrolling': 'no',
+	'titleShow': false
+    });
+}
+
+function init_node_list() {
+    $("#content .node").click(function () {
+	var self = $(this);
+	var node_id = $(this).attr("class").replace(/.*node_(\d+).*/, "$1");
+	$.fn.huddle.selectNode(node_id);
+    });
+
+    var height = $(window).height();
+    height -= $("#tree_nodes").offset().top;
+    var bottom = $("#tree_nodes").outerHeight() + $("#tree_nodes").offset().top;
+    bottom = $(document).height() - bottom;
+    height -= bottom;
+    console.log("height: " + height);
+    $("#tree_nodes").css("height", "" + height + "px");
 }
 
 function closeAddNodeForm(request) {
@@ -68,16 +91,42 @@ function addNode(nodeJson) {
 	$parentNode = nodeJson.parent_id > 0 ? $('#nodes #node_' + nodeJson.parent_id) : $('#nodes div:first');
 	var $generation = $parentNode.siblings("ol");
 	if ($generation.length == 0) {
+		console.log("generation is length 0"); // leaf node
 		$generation = $('<ol/>');
 		$parentNode.parent().append($generation);
 	}
 	var $nodeDataCloned = $parentNode.clone();
 	populateNodeDetailsWithResponse($nodeDataCloned, nodeJson);
 	$('<li/>').append($nodeDataCloned).appendTo($generation);
+
+	add_node_in_content(nodeJson);
+
 	if (nodeIdAdded == nodeJson.id) {
 		$.fn.huddle.selectNode(nodeJson.id);
 	}
 	$.fn.huddle.redraw();
+}
+
+function add_node_in_content(nodeJson) {
+    var parent = nodeJson.parent_id > 0 ? $(".node.node_" + nodeJson.parent_id) : $("#content .node").eq(0);
+
+    var node = $("<div/>")
+	.addClass("node")
+	.addClass("node_" + nodeJson.id);
+    var title_and_author = $("<div/>").addClass("title_and_author");
+    var title = $("<span/>").addClass("title").append(nodeJson.title);
+    var author = $("<spanr/>").addClass("author")
+	.append(nodeJson.user.login);
+    title_and_author.append(title).append("\n").append(author);
+    var avatar = $("<div/>").addClass("avatar");
+    var img = $("<img/>")
+	.attr("src", nodeJson.avatar_url ? nodeJson.avatarUrl : "/images/missing.png")
+	.attr("alt", "");
+    avatar.append(img);
+    var description = $("<description/>").addClass("description")
+	.append(nodeJson.description);
+    node.append(title_and_author).append(avatar).append(description);
+    parent.after(node);
 }
 
 function updateZoom(cbObj) {
@@ -85,16 +134,25 @@ function updateZoom(cbObj) {
 }
 
 function nodeSelected($nodeData) {
-	populateNodeDetails($nodeData);
+    var node = $(".node." + $nodeData.attr("id"));
+
+    $(".node").removeClass("selected");
+    node.addClass("selected");
+    var idx = $("#tree_nodes .node").index(node);
+    var scrollTo = Math.max(0, (idx - 2) * $(node).outerHeight());
+    $("#tree_nodes").animate({"scrollTop": scrollTo});
+    populateNodeDetails($nodeData);
 }
 
 function populateNodeDetails($nodeData) {
 	var $nodeDataCloned = $nodeData.clone();
 	$('#node_contents').hide().html("").append($nodeDataCloned).fadeIn(160);
 	var hieght = $(window).height() - 150;
+	/*
 	if ($('#nodes_wrapper').is(':hidden')) {
 		$('#nodes_wrapper').stop().show().animate({top: hieght}, {queue:false, duration:160});
 	}
+	*/
 	if (!$nodeData.data("cached")) {
 		$.getJSON("/nodes/" + $nodeDataCloned.attr("id").replace("node_", ""), function(response) {
 			populateNodeDetailsWithResponse($nodeDataCloned, response.node);
@@ -107,6 +165,8 @@ function populateNodeDetails($nodeData) {
 	}
 }
 
+// fill in a node html structure with values from "responseNode", which is the
+// json returned to us by the server -- EAW
 function populateNodeDetailsWithResponse($nodeDataCloned, responseNode) {
 	setReplyLink(responseNode.id);
 	$nodeDataCloned.attr('id', 'node_' + responseNode.id);
@@ -125,6 +185,7 @@ function populateNodeDetailsWithResponse($nodeDataCloned, responseNode) {
 	$nodeDataCloned.children('.comments').children().hide();
 }
 
+// sets the parent id for the new node form -- EAW
 function setReplyLink(parentId) {
 	$('#nodes_wrapper').find('.reply a').attr("href", "/nodes/new?parent=" + parentId);
 }
